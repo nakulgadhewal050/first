9f665cd74d360c6157c42f336151958c88fd87f2
nakul gadhewal <nakulgadhewal050@gmail.com>
2026-01-22 11:22:31 +0530

    nested rounting

--- src/app/about/contact/layout.js
+++ src/app/about/contact/layout.js
@@ -1,11 +1,10 @@
-import "./globals.css";
-
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body >
+        <div>
+            This is the contact layout
+        </div>
         {children}
       </body>
     </html>
