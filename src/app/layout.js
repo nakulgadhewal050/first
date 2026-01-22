2016dc07d4597423f5faa9ef7eb8d608960cb8d6
nakul gadhewal <nakulgadhewal050@gmail.com>
2026-01-22 16:01:05 +0530

    pareller routing

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,17 @@ import "./globals.css";
 
 
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, team, analytics }) {
   return (
     <html lang="en">
-      <body >
-        {children}
+      <body className="flex w-screen h-screen">
+        <div className="w-[50%] bg-red-200">{children}</div>
+        <div className="w-[50%] bg-blue-200">
+          {team}
+        </div>
+        <div className="w-[50%] bg-green-200">
+          {analytics}
+        </div>
       </body>
     </html>
   );
